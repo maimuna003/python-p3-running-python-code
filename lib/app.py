7efafdb1c4f35ec3c61e7df9021287c976eefa72
maimuna <mohamudmaimuna003@gmail.com>
2023-08-18 13:02:58 +0300

    done

---
+++ lib/app.py
@@ -0,0 +1,27 @@
+print("Hello World! Pass this test, please.")
+
+student_details =[{
+'fist_name':'ken',
+'last_name':'Abdi',
+'isactive':'False',
+'phases' : ['phase1','phase2','phase3']
+},
+{
+'fist_name':'ken',
+'last_name':'Abdi',
+'isactive':'False',
+'phases' : ['phase1','phase2','phase3']},
+
+{
+'fist_name':'ken',
+'last_name':'Abdi',
+'isactive':'False',
+'phases' : ['phase1','phase2','phase3']
+},
+]
+
+print(student_details[2])
+
+num = [1,2,3,4,5,6,7,8]
+num2 =set(num)
+print(num2)
